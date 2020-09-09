0caffff05192be0a38186b7c2d34508d3bcd5e07
Javier Serna <javiserna7@gmail.com>
2020-09-09 03:40:38 +0200

    Updates

---
+++ precache-manifest.cf69045f3eb03789fdc89fac3c0414b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d268436a1c0cc3d7dface92586a883",
+    "url": "/SPA-React/index.html"
+  },
+  {
+    "revision": "e56ad2cc8582c3361dc6",
+    "url": "/SPA-React/static/css/main.0e012e29.chunk.css"
+  },
+  {
+    "revision": "753b28e21f788f06f3de",
+    "url": "/SPA-React/static/js/2.2311b44b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SPA-React/static/js/2.2311b44b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56ad2cc8582c3361dc6",
+    "url": "/SPA-React/static/js/main.b8d1689a.chunk.js"
+  },
+  {
+    "revision": "bf96b96e8b1371c51d30",
+    "url": "/SPA-React/static/js/runtime-main.f6aca9ff.js"
+  }
+]);
